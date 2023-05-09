1aa421fbd5385d3849b9f73805e6017c98b9330f
anhlt4tv <anhlt4@tinhvan.com>
2023-05-09 14:11:14 +0700

    Update user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+    has_many :microposts
+    # validates FILL_IN, precense: true
+    # FILL_IN is a placeholder, replace FILL_IN with the name of the column you want validated
+    # Ex: validates :title, presence: true
+    validates :name, length: { maximum: 2 }
 end
